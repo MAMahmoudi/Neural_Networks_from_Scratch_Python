d19af264bc855c6156692c7f62e9c4074b6cc26e
MAMahmoudi <mohamed.mahmoudi@univ-mascara.dz>
2022-06-18 00:55:34 +0100

    I don't know

--- main.py
+++ main.py
@@ -8,3 +8,5 @@ from src.Layer_Dropout import Layer_Dropout
 from src.Layer_Dense import Layer_Dense
 from src.Layer_Activation import Activation_ReLU, Activation_Softmax
 from src.Layer_Input import Layer_Input
+from src.Optimizers import Optimizer_SGD, Optimizer_Adam,Optimizer_Adagrad, Optimizer_RMSprop
+from src.Loss import Loss_BinaryCrossentropy, Loss_CategoricalCrossentropy, Loss_MeanSquaredError, Loss_MeanAbsoluteError,Activation_Softmax_Loss_CategoricalCrossentropy
